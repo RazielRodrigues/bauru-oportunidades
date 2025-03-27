f61d7b4673cb8402c29892df09927634e70fc903
Raziel Rodrigues <razielx3@live.com>
2025-03-27 13:55:34 +0000

    new project

--- app/run.sh
+++ app/run.sh
@@ -1,4 +1,4 @@
-composer install
-php bin/console doctrine:database:create --if-not-exists
-php bin/console doctrine:migrations:migrate --no-interaction
-php bin/console doctrine:fixtures:load
+docker exec -it bauru_oportunidades_php sh composer install
+docker exec -it bauru_oportunidades_php sh php bin/console doctrine:database:create --if-not-exists
+docker exec -it bauru_oportunidades_php sh php bin/console doctrine:migrations:migrate --no-interaction
+docker exec -it bauru_oportunidades_php sh php bin/console doctrine:fixtures:load
